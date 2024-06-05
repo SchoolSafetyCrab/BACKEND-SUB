4755d98c0c7812f767f7cd88636466bfaba7cfe2
limjonghyeok <jjognbbang2@naver.com>
2024-06-05 16:19:45 +0900

    init

---
+++ BACKEND-SUB/src/main/java/com/backendsub/BackendSubApplication.java
@@ -0,0 +1,13 @@
+package com.backendsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSubApplication.class, args);
+	}
+
+}
---
+++ BACKEND-SUB/src/test/java/com/backendsub/BackendSubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
