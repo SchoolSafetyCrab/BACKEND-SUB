7457fcaec78ed0f57e282fe7b010ead104427f18
honggyeonghyeon <hyeon21owo@gmail.com>
2024-06-24 00:35:03 +0900

    AccidentSiteRepository 수정

--- BACKEND-SUB/src/main/java/com/backendsub/domain/accidentsite/repository/AccidentSiteRepositoryCustom.java
+++ BACKEND-SUB/src/main/java/com/backendsub/domain/accidentsite/repository/AccidentSiteRepositoryCustom.java
@@ -1,10 +1,11 @@
 package com.backendsub.domain.accidentsite.repository;
 
 import com.backendsub.domain.accidentsite.model.AccidentSite;
+import com.backendsub.domain.accidentsite.requestDto.AccidentSiteRequestDto;
 
 import java.util.List;
 
 public interface AccidentSiteRepositoryCustom {
 
-    List<AccidentSite> findNearByLocation(double latitude, double longitude, double radius);
+    List<AccidentSite> findNearByLocation(AccidentSiteRequestDto requestDto);
 }
--- BACKEND-SUB/src/main/java/com/backendsub/domain/accidentsite/repository/AccidentSiteRepositoryImpl.java
+++ BACKEND-SUB/src/main/java/com/backendsub/domain/accidentsite/repository/AccidentSiteRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.backendsub.domain.accidentsite.repository;
 
 import com.backendsub.domain.accidentsite.model.AccidentSite;
 import com.backendsub.domain.accidentsite.model.QAccidentSite;
+import com.backendsub.domain.accidentsite.requestDto.AccidentSiteRequestDto;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -16,13 +17,13 @@ public class AccidentSiteRepositoryImpl implements AccidentSiteRepositoryCustom{
     private QAccidentSite accidentSite = QAccidentSite.accidentSite;
 
     @Override
-    public List<AccidentSite> findNearByLocation(double latitude, double longitude, double radius) {
+    public List<AccidentSite> findNearByLocation(AccidentSiteRequestDto requestDto) {
         return queryFactory
                 .selectFrom(accidentSite)
                 .where(
                         Expressions.booleanTemplate(
                                 "ST_Distance_Sphere(POINT({0}, {1}), POINT({2}, {3})) <= {4}",
-                                longitude, latitude,
+                                requestDto.getLongitude(), requestDto.getLatitude(),
                                 accidentSite.longitude, accidentSite.latitude,
                                 5000
                         )
@@ -31,7 +32,7 @@ public class AccidentSiteRepositoryImpl implements AccidentSiteRepositoryCustom{
                         Expressions.numberTemplate(
                                 Double.class,
                                 "ST_Distance_Sphere(POINT({0}, {1}), POINT({2}, {3}))",
-                                longitude, latitude,
+                                requestDto.getLongitude(), requestDto.getLatitude(),
                                 accidentSite.longitude, accidentSite.latitude
                         ).asc()
                 )
