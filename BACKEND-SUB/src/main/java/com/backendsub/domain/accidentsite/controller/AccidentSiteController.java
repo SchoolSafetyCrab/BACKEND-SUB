11b195f8cb03c0e1981331b2808e158958a235a3
honggyeonghyeon <hyeon21owo@gmail.com>
2024-06-24 02:01:22 +0900

    AccidentSiteController 생성

---
+++ BACKEND-SUB/src/main/java/com/backendsub/domain/accidentsite/controller/AccidentSiteController.java
@@ -0,0 +1,30 @@
+package com.backendsub.domain.accidentsite.controller;
+
+import com.backendsub.domain.accidentsite.model.AccidentSite;
+import com.backendsub.domain.accidentsite.requestDto.AccidentSiteRequestDto;
+import com.backendsub.domain.accidentsite.service.AccidentSiteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class AccidentSiteController {
+
+    private final AccidentSiteService accidentSiteService;
+
+    @GetMapping("/accidentsite")
+    public ResponseEntity<?> findAccidentSite(@ModelAttribute AccidentSiteRequestDto requestDto) {
+
+        List<AccidentSite> responses = accidentSiteService.findNearByLocation(requestDto);
+//        ResponseEntity<Map<String, Object>> response =
+
+    }
+}
