863a5b4bd59330b2b292043c4f1357db3684d069
honggyeonghyeon <hyeon21owo@gmail.com>
2024-06-25 10:36:49 +0900

    EmergencyAlarmService 생성

---
+++ BACKEND-SUB/src/main/java/com/backendsub/domain/emergencyalarm/service/EmergencyAlarmService.java
@@ -0,0 +1,25 @@
+package com.backendsub.domain.emergencyalarm.service;
+
+import com.backendsub.domain.emergencyalarm.model.EmergencyAlarm;
+import com.backendsub.domain.emergencyalarm.repository.EmergencyAlarmRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class EmergencyAlarmService {
+
+    private final EmergencyAlarmRepository emergencyAlarmRepository;
+
+    @Transactional
+    public List<EmergencyAlarm> findEmergencyAlarmNearByLocation(long latitude, long longitude) {
+        List<EmergencyAlarm> responses = emergencyAlarmRepository.findNearByLocation(latitude, longitude);
+        return responses;
+    }
+}
